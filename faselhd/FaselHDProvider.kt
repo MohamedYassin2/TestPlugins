9f1580d3da5c5587ce2755b18b9b0cc132af74fb
MohamedYassin2 <mohamedyassin.s2002@gmail.com>
2025-07-26 21:42:41 +0200

    Create FaselHDProvider.kt

---
+++ faselhd/FaselHDProvider.kt
@@ -0,0 +1,43 @@
+package com.mohamedshihaa.faselhd
+
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.utils.AppUtils.toRatingInt
+import org.jsoup.nodes.Element
+
+class FaselHDProvider : MainAPI() {
+    override var mainUrl = "https://faselhd.link" // تأكد من الدومين الشغال
+    override var name = "FaselHD"
+    override var lang = "ar"
+    override val hasMainPage = false
+    override val supportedTypes = setOf(TvType.Movie)
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val url = "$mainUrl/search/${query.fixSearch()}"
+        val document = app.get(url).document
+
+        return document.select("div.GridItem.Card").mapNotNull {
+            it.toSearchResponse()
+        }
+    }
+
+    private fun Element.toSearchResponse(): MovieSearchResponse? {
+        val aTag = selectFirst("a") ?: return null
+        val href = fixUrl(aTag.attr("href"))
+        val img = selectFirst("img") ?: return null
+        val title = selectFirst("h3.Title")?.text()?.trim() ?: return null
+        val posterUrl = fixUrl(img.attr("data-src"))
+
+        return MovieSearchResponse(
+            title = title,
+            url = href,
+            apiName = this@FaselHDProvider.name,
+            type = TvType.Movie,
+            posterUrl = posterUrl
+        )
+    }
+
+    private fun String.fixSearch(): String {
+        return this.trim().replace(" ", "-")
+    }
+}
