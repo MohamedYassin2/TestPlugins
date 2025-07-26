bc2d8fe0dc51eae19abde9c3b31385b96e62df07
MohamedYassin2 <mohamedyassin.s2002@gmail.com>
2025-07-26 22:16:29 +0200

    Create build.gradle.kts

---
+++ faselhd/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+cloudstream {
+    language = "ar"
+    description = "FaselHD Movies and Series"
+    authors = listOf("Mohamed Shiha")
+
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+}
